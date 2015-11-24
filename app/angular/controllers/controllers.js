8301d1d6e663d24b63351f03ffb1b4bd5edd4765
Roman Spazm <troyzemmann@gmail.com>
2015-11-25 02:04:45 +0300

    NAVIGATION FIXED

--- app/angular/controllers/controllers.js
+++ app/angular/controllers/controllers.js
@@ -3,60 +3,65 @@ app.controller('galleryCtrl', function($scope, $http) {
     //Переменные среды
     //Индекс первого полного изображения (изначально скрыто) из списка
     $scope.fullImage = 0;
+    $scope.fullImagesBlock = document.querySelector('#toPlaceFullImages');
 
     //Загружаем данные картинок из файла в массив
     $http.get('./data.json').success(function (data) {
+
+        //Переменные среды
         $scope.images = data;
+        
     });
 
     //Свитчер показать/скрыть темный фон, изображение, навигацию
     $scope.wrapperShow = true;
-    
-    //Устанавливаем полное изображение через вставку индекса из массива изображений
-    //Вставка нужного индекса меняет атрибут ng-src и подгружает нужное изображение
-    $scope.setFullImage = function(index) {
-        //Устанавливаем нужный индекс
-        $scope.fullImage = index;
-        //Показываем темный фон и само изображение с навигацией
+
+    $scope.showBack = function () {
         $scope.wrapperShow = !$scope.wrapperShow;
     };
+    
+    $scope.setFullImage = function(index, step) {
+        //Цепляем в массив подгруженные полные изображения
+        $scope.fullImages = document.querySelectorAll('.image-gallery__full-image');
 
-    //Навигация
-    $scope.navSetImage = function(index, step) {
-        switch(step) {
-            case 'prev':
-                //Если перешли к самому первому изображению, переключаемся на последнее
-                index <= 0 ? $scope.fullImage = $scope.images.length - 1 : $scope.fullImage = index - 1;
-                //Во время загрузки снижаем прозрачность изображения
-                $scope.loading('#fullImg');
-                break;
-            case 'next':
-                //Если перешли к самому последнему изображению, переключаемся на первое
-                index === $scope.images.length - 1 ? $scope.fullImage = 0 : $scope.fullImage = index + 1;
-                //Во время загрузки снижаем прозрачность изображения
-                $scope.loading('#fullImg');
-                break;
+        //Цепляем текущее активное полное изображение
+        $scope.activeImage = document.querySelector('.image-gallery__full-image.active');
+
+        //Скрываем текущее активное полное изображение
+        if($scope.activeImage) {
+            $scope.activeImage.classList.remove('active');
+        }
+
+        //Заносим в переменную индекс текущего активного изображения
+        $scope.fullImageIndex = index;
+        $scope.fullImages[index].classList.add('active');
+
+        //Если передан параметр перемещения с навигации
+        if(step) {
+            //Скрываем установленное ранее активное полное изображение
+            $scope.fullImages[index].classList.remove('active');
+
+            //Навигация
+            switch(step) {
+                //Предыдущее
+                case 'prev':
+                    //Если дошли до первого изображения, переключаемся на последнее
+                    $scope.fullImageIndex <= 0 ? $scope.fullImageIndex = $scope.fullImages.length -1 : $scope.fullImageIndex--;
+                    //console.log($scope.fullImageIndex);
+                    //Показываем предыдущее изображение, делая его активным
+                    $scope.fullImages[$scope.fullImageIndex].classList.add('active');
+                    break;
+                //Следующее
+                case 'next':
+                    //Если дошли до последнего изображения, возвращаемся к первому
+                    $scope.fullImageIndex === $scope.fullImages.length - 1 ? $scope.fullImageIndex = 0 : $scope.fullImageIndex++;
+                    
+                    //console.log($scope.fullImageIndex);
+                    //Показываем следующее изображение, делая его активным
+                    $scope.fullImages[$scope.fullImageIndex].classList.add('active');
+                    break;
+            }
         }
     };
 
-    //Функция снижения прозрачности при подгрузке изображения
-    //Запускается каждый раз, когда меняется атрибут ng-src у основного (полного) изображения
-    $scope.loading = function (imageId) {
-
-        //Цепляем состояние DOM-элемента картинки
-        $scope.mainImage = document.querySelector(imageId);
-        //Устанавливаем слушатель
-        $scope.$watch('mainImage', function () {
-
-            //Снижаем прозрачность
-            $scope.mainImage.style.opacity = '.1';
-            
-            //При полной загрузке возвращаем прозрачность к 100%
-            $scope.mainImage.onload = function () {
-                this.style.opacity = '1';
-            }
-
-        });
-
-    }
 });
